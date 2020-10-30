b9781a61eb1b51f06a1b975ac88754bbb86ca4bf
Arturo Fernández <kevin.fernandez4579@alumnos.udg.mx>
2020-10-29 18:40:27 -0600

    En el archivo main.cpp, creamos una instancia de la clase arreglo; insertamos 8 cadenas al final y mostramos el contenido del arreglo. Posteriormente, insertamos en el inicio del arreglo e igualmente mostramos el contenido, teniendo así un total de 10 elementos(cadena) registradas.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include "arreglo.h"
 
 using namespace std;
 
 int main(){
+
+    Arreglo myArray;
+
+    myArray.insertarFinal("Einstein");
+    myArray.insertarFinal("Tesla");
+    myArray.insertarFinal("Feynman");
+    myArray.insertarFinal("Newton");
+    myArray.insertarFinal("Bohr");
+    myArray.insertarFinal("Galilei");
+    myArray.insertarFinal("Shell");
+    myArray.insertarFinal("Babbage");
+    
+    for (size_t i = 0; i < myArray.size(); i++)
+    {
+        cout << myArray[i] << endl;
+    }
+
+    myArray.insertarInicio("Turing");
+    myArray.insertarInicio("Ada");
+
+    cout << endl << endl;
+
+    for (size_t i = 0; i < myArray.size(); i++)
+    {
+        cout << myArray[i] << endl;
+    }
 
     return 0;
 }
